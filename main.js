5a169118d7dbdc2d796be1e04c66274a138c280e
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-06-12 10:16:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function setup() {
     canvas = createCanvas(500, 360);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 500, 360);
+}
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting Objects";
+    object = document.getElementById("objects").value;
+}
+status_ = ""; 
+object = "";
+function modelLoaded() {
+    status_ = true;
+    console.log("model loaded");
 }
